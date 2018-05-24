c4f806463d0e3819fba8682cf4a682a3d417f5b8
Juan Viera <juan.viera@tecnoandina.cl>
2018-05-24 14:22:54 -0400

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: Http, public navCtrl: NavController) {
+
+
+    this.probarApi();
 
   }
 
+  getPartidasByCode(){
+    
+    return new Promise ((Resolve,reject) => {
+
+      this.http.get("http://192.168.100.14:2509/prueba/gt/1234", JSON.stringify({}))
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          console.log(data);
+          Resolve(data);
+          //resolve(data['planificacion']);
+        },
+        error => {
+          console.log(error);
+          reject(error);
+        }
+      )
+
+    });
+
+  }
+
+  async probarApi(){
+    await this.getPartidasByCode();
+  }
 }
